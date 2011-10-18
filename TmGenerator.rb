c6fae444174a9431c33bb219c82536e3633de50d
Mark Doyle <markjohndoyle@googlemail.com>
2011-10-18 23:02:12 +0200

    Crappy TM generator.

--- TmGenerator.rb
+++ TmGenerator.rb
@@ -1,6 +1,18 @@
 print "Generating TM\n"
 
+maxFuel = 1000
+maxLaser = 2040
 for i in (0..100)
-  laserTemp = i.to_s(2).rjust(11, '0')
-  print laserTemp + "\n"
+  laserTempVal = Math.sin(i) * 100
+  if (laserTempVal < 0)
+    laserTempVal = 0;
+  end
+  laserTemp = laserTempVal.floor.to_s(2).rjust(11, '0')
+  fuel = maxFuel.to_s(2).rjust(16, '0')
+  maxFuel = maxFuel - 10
+  flightHours = i.to_s(2).rjust(32, '0')
+  
+
+  tm = laserTemp + fuel + flightHours
+  print tm + "\n"
 end
